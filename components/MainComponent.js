e18229d7bb599f8f9ec490d4b35c99da1a6f921c
AmmarKhalid123 <ammarkhalid237@gmail.com>
2020-04-23 04:13:43 +0500

    React Navigation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
+import Dishdetail from './DishDetailComponent';
+import { View, Platform } from 'react-native';
+import {createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+const MenuNavigator = createStackNavigator({ 
+    Menu: { screen: Menu },
+    Dishdetail: { screen: Dishdetail } 
+}, {
+    initialRouteName: 'Menu',
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: '#512DA8',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
+        }
+    }
+});
+
+const MenuNavigatorApp = createAppContainer(MenuNavigator);
 
 class Main extends Component {
 
-        state = {
-            dishes: DISHES
-        };
-    
-
     render() {
         return(
-            <Menu dishes={this.state.dishes} />
+            <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight}}>
+            <MenuNavigatorApp />
+             </View>
         );
     }
 }
